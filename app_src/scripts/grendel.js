4a8f91d6b523b1b60919bf891992a7632b46da99
dfg129 <dfg129@gmail.com>
2014-12-13 21:47:08 -0500

    flexbox

--- app_src/scripts/grendel.js
+++ app_src/scripts/grendel.js
@@ -1,37 +1,37 @@
 // declare module
 
-var appModule = angular.module('grendel', ['ui-router']);
+var appModule = angular.module('appModule', ['ui.router']);
 
-appModule.config(function($stateProvider, $urlRouteProvider) {
+appModule.config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
     .state('main', {
       url: "/main",
-      templateUrl: "partials/main.html"
+      templateUrl: "./partials/main.html"
     })
     .state('state1', {
       url: "/state1",
-      templateUrl: "partials/state1.html",
+      templateUrl: "./partials/state1.html",
       controller: 'State1Controller'
     })
     .state('state1.list', {
       url:"/list",
-      templateUrl:"partials/state1.list.html",
+      templateUrl:"./partials/state1.list.html",
       controller: function($scope) {
         $scope.items = ["A", "List", "Of", "Items"];
       }
     })
     .state('state2', {
       url: "/state2",
-      templateUrl: "partials/state2.html",
+      templateUrl: "./partials/state2.html",
       controller: 'State2Controller'
     })
     .state('state2.list', {
       url:"/list",
-      templateUrl:"partials/state2.list.html",
+      templateUrl:"./partials/state2.list.html",
       controller: function($scope) {
         $scope.things = ["A", "Set", "Of", "Things"];
       }
-    });
-    
-    $urlRouteProvider.otherwise('/settings/profile');
-});
+    })
+
+    $urlRouterProvider.otherwise('/')
+  });
