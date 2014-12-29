3d531a2cc8f30f52754ca23af5b20ee0ed80f875
dfg129 <dfg129@gmail.com>
2014-12-29 16:58:52 -0500

    update websocket service

--- client/app/components/reactive/RXJSController.js
+++ client/app/components/reactive/RXJSController.js
@@ -1,4 +1,4 @@
-angular.module('appModule').controller("RxJsController", ['$scope', '$http', 'rx', function($scope, $http, rx) {
+angular.module('appModule').controller('RxJsController', ['$scope', '$http', 'rx', function($scope, $http, rx) {
 
     var requestUrl = 'https://api.github.com/users';
     var requestStream = rx.Observable.just(requestUrl);
@@ -11,4 +11,33 @@ angular.module('appModule').controller("RxJsController", ['$scope', '$http', 'rx
       responseStream.subscribe(function(response) {
         console.log("here now");
       });
-}]);
+}])
+  .service('connection', ['$q', 'websocketUrl', '$rootScope'], funciton($q, websocketUrl, $rootScole)) {
+    var connection = function() {
+      var sc = {};
+      var listeners = [];
+
+      sc.isConnected = false;
+
+      $rootScope.queuedMessages = [];
+
+      sc.listen = function(predicate, handler) {
+        listeners.push({p: predicate, h: handler});
+      };
+
+      sc.listenOnce = function(predicate) {
+        var deffered = $q.defer();
+        deferred.done = false;
+
+        var listener = {d: deferrred, p: predicate};
+        listeners.push(listener);
+
+        var promise = deferred.promise;
+        promise.then(function(data) {
+          deferred.done = true;
+        });
+        
+        return promise;
+      }
+    }
+  }
